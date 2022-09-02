62ca02abc68b42772ffc44bc9408bfb97a908d8f
dev-parthive <dev-parthive>
2022-09-03 00:07:00 +0600

    product show in ui

--- js/app.js
+++ js/app.js
@@ -41,9 +41,48 @@ setAllMenu()
 // loadAllProducts()
 
 const searchField = document.getElementById('search-field'); 
-searchField.addEventListener('keypress',  async(event)=>{
+searchField.addEventListener('keypress',  async (event)=>{
     // console.log(event.key);
     if(event.key === 'Enter'){
-        console.log(searchField.value)
+        // console.log(searchField.value)
+        const searchValue = searchField.value ;
+        const allProducts =  await loadAllProducts();
+        // console.log(allProducts)
+        const foundProducts = allProducts.filter(product => product.category.includes(searchValue))
+        console.log(foundProducts)
+        const prodcutsContainer =  document.getElementById('products-container');
+        prodcutsContainer.textContent = ''
+
+
+        // No prodcuts to show message 
+        if(foundProducts.length === 0 ){
+            prodcutsContainer.innerHTML = `
+            
+            <h3 class="text-center" style="font-size: 40px; "> No products to show </h3>
+            
+            `
+        }
+
+        
+        foundProducts.forEach(product => {
+            console.log(product)
+            const {category, image, title} = product
+
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="card card-compact w-full bg-base-100 shadow-xl">
+  <figure><img src="${image}" alt="Shoes" class="h-60 w-full" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">${category}!</h2>
+    <p>${title.length > 20 ? title.slice(0,20) + '....': title}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Show Details</button>
+    </div>
+  </div>
+</div>
+            
+            `;
+            prodcutsContainer.appendChild(div)
+        });
     }
 })
